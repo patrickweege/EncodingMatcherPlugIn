35f013488bae9022e3b12b7ece9326fb4fdb22ae
patrickweege <patrickweege@gmail.com>
2015-10-20 21:39:18 -0200

    Changes

--- EncodingMatcherPlugIn/src/com/pweege/plugins/encoding/matcher/builder/EncodingMatcherPluginBuilder.java
+++ EncodingMatcherPlugIn/src/com/pweege/plugins/encoding/matcher/builder/EncodingMatcherPluginBuilder.java
@@ -1,11 +1,9 @@
 package com.pweege.plugins.encoding.matcher.builder;
 
+import java.io.InputStream;
 import java.util.Map;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
@@ -16,80 +14,51 @@ import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
-import org.xml.sax.helpers.DefaultHandler;
+
+import com.ibm.icu.text.CharsetMatch;
+import com.pweege.plugins.encoding.matcher.util.EncodingMatcherUtil;
 
 public class EncodingMatcherPluginBuilder extends IncrementalProjectBuilder {
 
-	class SampleDeltaVisitor implements IResourceDeltaVisitor {
+	class IncrementalBuildDeltaVisitor implements IResourceDeltaVisitor {
 		/*
 		 * (non-Javadoc)
 		 * 
-		 * @see org.eclipse.core.resources.IResourceDeltaVisitor#visit(org.eclipse.core.resources.IResourceDelta)
+		 * @see
+		 * org.eclipse.core.resources.IResourceDeltaVisitor#visit(org.eclipse.
+		 * core.resources.IResourceDelta)
 		 */
 		public boolean visit(IResourceDelta delta) throws CoreException {
 			IResource resource = delta.getResource();
 			switch (delta.getKind()) {
 			case IResourceDelta.ADDED:
-				// handle added resource
 				checkEncoding(resource);
 				break;
 			case IResourceDelta.REMOVED:
 				// handle removed resource
 				break;
 			case IResourceDelta.CHANGED:
-				// handle changed resource
 				checkEncoding(resource);
 				break;
 			}
-			//return true to continue visiting children.
+			// return true to continue visiting children.
 			return true;
 		}
 	}
 
-	class SampleResourceVisitor implements IResourceVisitor {
+	class FullBuildResourceVisitor implements IResourceVisitor {
 		public boolean visit(IResource resource) {
 			checkEncoding(resource);
-			//return true to continue visiting children.
+			// return true to continue visiting children.
 			return true;
-		}
-	}
-
-	class XMLErrorHandler extends DefaultHandler {
-		
-		private IFile file;
-
-		public XMLErrorHandler(IFile file) {
-			this.file = file;
-		}
-
-		private void addMarker(SAXParseException e, int severity) {
-			EncodingMatcherPluginBuilder.this.addMarker(file, e.getMessage(), e
-					.getLineNumber(), severity);
-		}
-
-		public void error(SAXParseException exception) throws SAXException {
-			addMarker(exception, IMarker.SEVERITY_ERROR);
-		}
-
-		public void fatalError(SAXParseException exception) throws SAXException {
-			addMarker(exception, IMarker.SEVERITY_ERROR);
-		}
-
-		public void warning(SAXParseException exception) throws SAXException {
-			addMarker(exception, IMarker.SEVERITY_WARNING);
 		}
 	}
 
 	public static final String BUILDER_ID = "EncodingMatcherPlugIn.encodingMatcherPluginBuilder";
 
-	private static final String MARKER_TYPE = "EncodingMatcherPlugIn.xmlProblem";
+	private static final String MARKER_TYPE = "EncodingMatcherPlugIn.encodingProblem";
 
-	private SAXParserFactory parserFactory;
-
-	private void addMarker(IFile file, String message, int lineNumber,
-			int severity) {
+	private void addMarker(IFile file, String message, int lineNumber, int severity) {
 		try {
 			IMarker marker = file.createMarker(MARKER_TYPE);
 			marker.setAttribute(IMarker.MESSAGE, message);
@@ -106,10 +75,9 @@ public class EncodingMatcherPluginBuilder extends IncrementalProjectBuilder {
 	 * (non-Javadoc)
 	 * 
 	 * @see org.eclipse.core.internal.events.InternalBuilder#build(int,
-	 *      java.util.Map, org.eclipse.core.runtime.IProgressMonitor)
+	 * java.util.Map, org.eclipse.core.runtime.IProgressMonitor)
 	 */
-	protected IProject[] build(int kind, Map args, IProgressMonitor monitor)
-			throws CoreException {
+	protected IProject[] build(int kind, Map args, IProgressMonitor monitor) throws CoreException {
 		if (kind == FULL_BUILD) {
 			fullBuild(monitor);
 		} else {
@@ -129,14 +97,50 @@ public class EncodingMatcherPluginBuilder extends IncrementalProjectBuilder {
 	}
 
 	void checkEncoding(IResource resource) {
-		if (resource instanceof IFile && resource.getName().endsWith(".xml")) {
-			IFile file = (IFile) resource;
-			deleteMarkers(file);
-			XMLErrorHandler reporter = new XMLErrorHandler(file);
-			try {
-				getParser().parse(file.getContents(), reporter);
-			} catch (Exception e1) {
+		try {
+			if (resource instanceof IFile) {
+				IFile file = (IFile) resource;
+				String currentCharset = this.getCurrentCharset(file);
+				
+				InputStream contents = file.getContents();
+				
+				boolean decodable = EncodingMatcherUtil.isDecodable(contents, currentCharset);
+				if(!decodable) {
+					contents = file.getContents();
+					CharsetMatch[] detectCharsets = EncodingMatcherUtil.detectCharsets(contents);
+					
+					StringBuilder altMessage = new StringBuilder();
+					int currentConfidence = 0;
+					boolean first = true;
+					for (CharsetMatch charsetMatch : detectCharsets) {
+						if(!first) {
+							altMessage.append("; ");
+						}
+
+						if(EncodingMatcherUtil.areCharsetsEqual(currentCharset, charsetMatch.getName())) {
+							currentConfidence = charsetMatch.getConfidence();
+						} else {
+							altMessage.append(charsetMatch.getName()).append("(").append(charsetMatch.getConfidence()).append("%");
+							first = false;
+						}
+					}
+					
+					StringBuilder msg = new StringBuilder();
+					msg.append("The File not Matches the configured Charset: ");
+					msg.append(currentCharset);
+					msg.append(" (");
+					msg.append(currentConfidence);
+					msg.append("%) - Other Alternatives are: ");
+					msg.append(altMessage);
+					
+					addMarker(file, msg.toString(), -1, IMarker.SEVERITY_WARNING);
+
+				} else {
+					deleteMarkers(file);
+				}
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -144,28 +148,30 @@ public class EncodingMatcherPluginBuilder extends IncrementalProjectBuilder {
 		try {
 			file.deleteMarkers(MARKER_TYPE, false, IResource.DEPTH_ZERO);
 		} catch (CoreException ce) {
+
 		}
 	}
 
-	protected void fullBuild(final IProgressMonitor monitor)
-			throws CoreException {
+	protected void fullBuild(final IProgressMonitor monitor) throws CoreException {
 		try {
-			getProject().accept(new SampleResourceVisitor());
+			getProject().accept(new FullBuildResourceVisitor());
 		} catch (CoreException e) {
 		}
 	}
 
-	private SAXParser getParser() throws ParserConfigurationException,
-			SAXException {
-		if (parserFactory == null) {
-			parserFactory = SAXParserFactory.newInstance();
-		}
-		return parserFactory.newSAXParser();
+	protected void incrementalBuild(IResourceDelta delta, IProgressMonitor monitor) throws CoreException {
+		// the visitor does the work.
+		delta.accept(new IncrementalBuildDeltaVisitor());
 	}
 
-	protected void incrementalBuild(IResourceDelta delta,
-			IProgressMonitor monitor) throws CoreException {
-		// the visitor does the work.
-		delta.accept(new SampleDeltaVisitor());
+	private String getCurrentCharset(IFile file) throws CoreException {
+		String theCharset = null;
+		theCharset = file.getCharset(false);
+		if (theCharset == null) {
+			IContainer container = file.getParent();
+			theCharset = container.getDefaultCharset(true);
+		}
+		return theCharset;
 	}
+
 }
